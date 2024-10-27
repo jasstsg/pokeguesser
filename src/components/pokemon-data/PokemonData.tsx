efa57e9fc9c5c91d0d615146d757acdc2a84e0aa
Tyler Jass <33812929+Tylernol2@users.noreply.github.com>
2024-10-27 15:33:32 -0400

    started styling a little

--- src/components/pokemon-data/PokemonData.tsx
+++ src/components/pokemon-data/PokemonData.tsx
@@ -1,4 +1,5 @@
 import { Pokemon } from "pokenode-ts";
+import './pokemon-data.css';
 
 
 function PokemonData(props: any) {
@@ -8,28 +9,38 @@ function PokemonData(props: any) {
     var form = nameData.length > 1 ? `(${nameData[1]})` : '';
 
     return (
-        <>
+        <div className="pokemon-data">
             <div className="flex-row">
-                <div className="bio flex-col">
+                <div className="basic-info flex-col">
+                    <h1 className="capitalize">{`${name} ${form}`}</h1>
+                    <img className="sprite" src={pokemon.sprites.front_default ?? ""} alt="pokemon image" />
+                    <div>{pokemon.stats.map(s => (
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>{s.stat.name}</td>
+                                    <td>{s.base_stat}</td>
+                                    <td>{s.effort}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    ))}
+                </div>
+                </div>
+                <div className="bio-info flex-col">
                     <div>Height: {pokemon.height / 10}m</div>
                     <div>Weight: {pokemon.weight / 10}kg</div>
                     <div>Types: {pokemon.types.map(t => t.type.name).join(",")}</div>
-                    <div>Abilities: {pokemon.abilities.map(a => a.ability.name).join(",")}</div>
+                    <div>Abilities: {pokemon.abilities.map(a => a.ability.name.replace("-", " ")).join(",")}</div>
                     <div>Forms: {pokemon.forms.map(f => f.name).join(",")}</div>
                     <div>Species: {pokemon.species.name}</div>
-                    <div>Location Area Encounters: {pokemon.location_area_encounters}</div>
-                    <div>Held Items: {pokemon.held_items.map(h => h.item.name).join(",")}</div>
+                    <div>Held Items: {pokemon.held_items.map(h => h.item.name.replace("-", " ")).join(",")}</div>
                 </div>
-                <div className="flex-col">
-                    <h1 className="capitalize">{`${name} ${form}`}</h1>
-                    <img className="sprite" src={pokemon?.sprites.front_default ?? ""} alt="pokemon image" />
-                    <div>Stats: {pokemon.stats.map(s => `${s.stat.name} (B:${s.base_stat}/EV:${s.effort})`).join(",")}</div>
-                </div>
-                <div className="flex-col">
-                    <div>Moves: {pokemon.moves.map(m => m.move.name).join(",")}</div>
+                <div className="moves-info flex-col">
+                    <div>Moves: {pokemon.moves.map(m => m.move.name.replace("-", " ")).join(",")}</div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
