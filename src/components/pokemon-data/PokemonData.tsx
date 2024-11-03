570f3223625ef710aa2e8ef366fb82ed6cdc52cb
Tyler Jass <jasstsg@gmail.com>
2024-11-03 01:18:33 -0400

    moved weight under the height, next to the pokemon sprites

--- src/components/pokemon-data/PokemonData.tsx
+++ src/components/pokemon-data/PokemonData.tsx
@@ -38,13 +38,15 @@ function PokemonData(props: any) {
                     <div className="bio-details grid-1-1">
                         <Stats stats={pokemon.stats}></Stats>
                         <div>
-                            <div className="text-center align-center grid-1-1-1">
-                                <div className="border-right h-full">{pokemon.height / 10}m</div>
+                            <div className="text-center grid-1-1-1">
+                                <div className="border-right h-full flex-col justify-center">
+                                    <div>{pokemon.height / 10}m</div>
+                                    <div>{pokemon.weight / 10}kg</div>
+                                </div>
                                 <img id="sprite-default" src={pokemon.sprites.front_default ?? ""} alt="pokemon default image" />
                                 <img id="sprite-shiny" src={pokemon.sprites.front_shiny ?? ""} alt="pokemon shiny image" />
                             </div>
                             <div>
-                                <div>Weight: {pokemon.weight / 10}kg</div>
                                 <div>Held Items: {pokemon.held_items.length > 0 ? pokemon.held_items.map(h => h.item.name.replace("-", " ")).join(",") : "None"}</div>
                                 <div>Abilities: {pokemon.abilities.map(a => a.ability.name.replace("-", " ")).join(", ")}</div>
                             </div>
